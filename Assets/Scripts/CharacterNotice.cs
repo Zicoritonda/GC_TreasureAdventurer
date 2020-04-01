2efe97a9bd1f59910c3ae7b56a115b2fe0dac12d
Zicoritonda <zicoritonda@yahoo.co.id>
2020-04-01 23:35:58 +0700

    update

---
+++ Assets/Scripts/CharacterNotice.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterNotice : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.name == "Enemy")
+        {
+            Debug.Log("hai");
+            transform.Find("Canvas").gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("bye");
+        transform.Find("Canvas").gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/NavWithCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavWithCollider : MonoBehaviour
+{
+    public string sceneName;
+    public string colliderName;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(this.gameObject.name + " collided with " + col.name);
+        if(col.name == colliderName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+    }
+}
